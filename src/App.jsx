2370ad2a04a79880b9f7c3017ee518c1c7086911
mdrakibmia99 <rkrakibhasan680@gmail.com>
2024-01-10 16:47:57 +0600

    update task count

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Carts from "./components/Carts"
 const DemoCartTitle = [
-  { title: "Complete", className: { textColor: "bg-red-500" }  , count:0},
-  { title: "To do", className: { textColor: "bg-blue-500" } , count:0 },
-  { title: "Doing", className: { textColor: "bg-yellow-500" }  , count:0},
-  { title: "Under Review", className: { textColor: "bg-red-500" } , count:0 },
-  { title: "Completed", className: { textColor: "bg-blue-500" } , count:0 },
-  { title: "Overview", className: { textColor: "bg-yellow-500" } , count:0 }
+  { title: "Complete", className: { textColor: "bg-red-500" }, count: 8 },
+  { title: "To do", className: { textColor: "bg-blue-500" }, count: 20 },
+  { title: "Doing", className: { textColor: "bg-yellow-500" }, count: 30 },
+  { title: "Under Review", className: { textColor: "bg-red-500" }, count: 0 },
+  { title: "Completed", className: { textColor: "bg-blue-500" }, count: 0 },
+  { title: "Overview", className: { textColor: "bg-yellow-500" }, count: 0 }
 
 ]
 
@@ -13,21 +13,27 @@ function App() {
 
 
   return (
-    <div className="w-full max-h-[700px] ">
-      <div className="flex gap-2 w-full max-h-[700px] overflow-scroll ">
+    <div className="w-full  overflow-hidden px-2">
+      <div className="flex gap-2 w-full h-screen overflow-x-scroll overflow-y-hidden">
 
-      {
-        DemoCartTitle.map((item, index) => (<div key={index} className="bg-slate-200">
-          <div className="sticky">
-            <div className="flex justify-between items-center px-3 py-5">
-              <div> <h1>{item.title}</h1></div>
-              <div className="w-5 h-5 rounded-md bg-slate-300 flex justify-center items-center"><p>{item.count}</p></div>
+        {
+          DemoCartTitle.map((item, index) => (<div key={index} className="bg-slate-200 w-full relative">
+            <div className="absolute top-0 w-full h-6  bg-slate-200 ">
+              <div className="flex justify-between items-center px-3 py-5">
+                <div className="w-full rounded-lg relative overflow-hidden">
+                  <div
+                    className={`absolute top-0 left-0 h-full ${item.className.textColor}`}
+                    style={{ width: `${item.count}%` }}
+                  ></div>
+                  <h1 className="relative z-10 pl-5">{item.title}</h1>
+                </div>
+                <div className="w-6 h-6 rounded-md bg-slate-300 flex justify-center items-center"><p>{item.count}</p></div>
+              </div>
             </div>
+            <Carts />
           </div>
-          <Carts />
-        </div>
-        ))
-      }
+          ))
+        }
       </div>
     </div>
   )
--- src/components/Carts.jsx
+++ src/components/Carts.jsx
@@ -4,7 +4,7 @@ const Carts = () => {
    const value=12;
     const demoCartArray = Array.from({ length: value }, (_, index) => index + 1);
     return (
-        <div className="flex flex-col gap-3 w-full h-full overflow-y-scroll">
+        <div className="flex flex-col gap-3 w-full h-full overflow-y-scroll mt-14">
             {
                 demoCartArray.map((item,index)=> <Cart key={index}/>)
             }
